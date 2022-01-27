5f0fb0f0c3c4c5a55b215cdb01e9271af2343e14
Ivan F. Clevo <iv20056@yahoo.it>
2022-01-27 21:03:53 +0000

    attached

---
+++ VueJScraper/src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="home">
+        <h1>{{ msg }}</h1>
+        <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a> and <a href="http://www.typescriptlang.org/" target="_blank">TypeScript</a>.</p>
+
+        <!-- slam input box for search phrase (land registry search) -->
+        <!-- and the url (www.infotrack.co.uk) -->
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class Home extends Vue {
+        @Prop() private msg!: string;
+        reply: string = "";
+
+        //https://localhost:44361/ScraperHome
+
+        created() {
+            console.log("created!!");
+
+            //Vue.$http.headers.common['Access-Control-Allow-Origin'] = true;
+            //Vue.$http.headers.common['Access-Control-Allow-Origin'] = '*';
+
+            this.scrapeTheWeb();
+        }
+
+        async scrapeTheWeb(): Promise<void> {
+            console.log("scraping...");
+
+            //const response = await fetch("https://localhost:44361/ScraperHome/GetScrapeInformation");
+            const response = await fetch("https://localhost:44361/ScraperHome/GetScrapeInfo/asd");
+            if (response.ok) {
+                console.log('ok!!');
+                const body = await response.text();
+                console.dir(body)
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ WebCoreScraper/Controllers/ScraperHome.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCoreScraper.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScraperHomeController : ControllerBase
+    {
+        public ScraperHomeController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome Scraper (backend)";
+        }
+
+        [HttpGet("GetScrapeInformation")]
+        public string GetScrapeInformation()
+        {
+            string id = "ciao";
+            return $"You asked id {id}";
+        }
+
+        [HttpGet("GetScrapeInfo/{id}")]
+        public string GetScrapeInfo(string id)
+        {
+            return $"You asked id {id}";
+        }
+    }
+}
